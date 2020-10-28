b5e1d798110c7e847ab5e9bff72eb5003154f298
adamabarrow <abarrow336@gmail.com>
2020-10-28 11:26:03 -0500

    commentsOutline

---
+++ comments/commentsOutline.py
@@ -0,0 +1,20 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+# this is my single line comment.
+
+#2) Make a multi-line comment.
+
+'''
+This is my multi-line comment
+I can write in more than one line
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+b = 4 #b is equivalent to 4
