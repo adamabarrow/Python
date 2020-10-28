880c3a12810ac8fa5988efb6e95b499c4067762d
adamabarrow <abarrow336@gmail.com>
2020-10-28 11:35:39 -0500

    commentDrills

---
+++ comments/commentDrills.py
@@ -0,0 +1,43 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#This is a single line comment. 
+
+#2) Make a multi-line comment.
+
+'''
+This is a multi-line comment. 
+I can write more than one line of comments.
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+b = 200 #The variable b is equivalent to 200
+
+#4) Make a single line comment.
+
+#I like doing the drills in this course.
+
+#5) Make a multi-line comment.
+
+'''
+I like doing the drills in this course.
+I like them because they are simple to follow.
+I can also get good practic while doing them.
+'''
